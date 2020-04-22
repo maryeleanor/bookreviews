b84f3897144b10c265a7d82b4f372c64ca8dc68a
maryeleanor <marycasserly@gmail.com>
2020-04-21 20:27:16 -0700

    added book pages and review input

--- app.py
+++ app.py
@@ -59,32 +59,124 @@ def apology(message, code=400):
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
+def search(book):
+    book = "%{}%".format(book)
+    books = db.execute("SELECT * FROM books WHERE isbn ILIKE :book OR title ILIKE :book OR author ILIKE :book ORDER BY year DESC", {"book": book}).fetchall()
+    if not books:
+        no_match = "Sorry, no books found. Try another search"
+        return render_template('index.html', no_match=no_match)
+    goodreads = []
+
+    for book in books:
+            isbn = book.isbn
+            res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                           params={"key": "DchANaDeTrPFG7BRpUBZIw", "isbns": isbn})
+            result = res.json()
+            goodreads.append(result['books'])
+
+    books_result = zip(books, goodreads)
+    return books_result
+
+def book_reviews(book_id):
+    book = db.execute(
+        "SELECT * FROM books WHERE id = :book_id", {
+            "book_id": book_id}).fetchone()
+
+    reviews = db.execute(
+        "SELECT * FROM reviews WHERE book_id = :book_id", {
+            "book_id": book.id}).fetchall()
+
+    user = db.execute("SELECT * FROM users WHERE id = :id",
+                      {"id": session["user_id"]}).fetchone()
+    username = user.username.capitalize()
+
+    isbn = book.isbn
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": "DchANaDeTrPFG7BRpUBZIw", "isbns": isbn})
+    result = res.json()
+    goodreads_book = result['books']
+    ratings = (float(goodreads_book[0]['average_rating'])/5) * 100
+    return book, reviews, username, goodreads_book, ratings
+
+
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     if request.method == "POST":
         book = request.form.get("book") 
-        search = "%{}%".format(book)
-        books = db.execute(
-            "SELECT * FROM books WHERE isbn ILIKE :book OR title ILIKE :book OR author ILIKE :book ORDER BY year DESC", {"book":search}).fetchall()
-        # books.append({'ratings_count':''})  
-        # books.append({'reviews_count': ''})
-        # books.append({'average_rating': ''})
-        for book in books:
-            isbn = book.isbn
-            res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                           params={"key": "DchANaDeTrPFG7BRpUBZIw", "isbns": isbn})
-            json_result=res.json() 
-            goodreads = json_result['books']
-            # book.ratings_count = goodreads['ratings_count']
-            # book.reviews_count = goodreads['reviews_count']
-            # book.average_ratings = goodreads['average_ratings']
-        return render_template('index.html', books=books, goodreads=goodreads)
+
+        if not book:
+            return apology("must provide a book to search for", 404)
+
+        books_result = search(book)
+        return render_template('index.html', books_result=books_result)
 
     else:
         user = db.execute("SELECT * FROM users WHERE id = :id", {"id":session["user_id"]}).fetchall()
         username = user[0].username.capitalize()
-        return render_template('index.html', username=username)
+        return render_template('index.html')
+
+
+
+@app.route('/book/<int:book_id>', methods=['GET', 'POST'])
+def book(book_id):
+    if request.method == "POST":
+        review = request.form.get("review")
+        rating = int(request.form['options'])
+
+        if not review and not rating:
+            return apology("you didn't provide a rating or review", 404)
+
+        db.execute("INSERT INTO reviews (review, rating, book_id, user_id) VALUES (:review, :rating, :book_id, :user_id)", {
+                   "review": review, "rating": rating, "book_id": book_id, "user_id": session["user_id"]})
+        db.commit()
+
+        book, reviews, username, goodreads_book, ratings = book_reviews(book_id)
+        return render_template('book.html', book=book, reviews=reviews, username=username, goodreads_book=goodreads_book, ratings=ratings)
+
+    else:
+        book, reviews, username, goodreads_book, ratings = book_reviews(book_id)
+        return render_template('book.html', book=book, reviews=reviews, username=username, goodreads_book=goodreads_book, ratings=ratings)
+
+
+@app.route('/review/<int:review_id>', methods=['GET', 'POST'])
+def review(review_id):
+    #  review = review.query.get_or_404(review_id)
+    return render_template('review.html', title=review.title, review=review)
+
+
+@app.route('/review/<int:review_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_review(review_id):
+    review = review.query.get_or_404(review_id)
+    if review.author != current_user:
+        abort(403)
+    form = reviewForm()
+    if form.validate_on_submit():
+        review.title = form.title.data
+        review.content = form.content.data
+        db.session.commit()
+        flash('Your review has been updated.', 'success')
+        return redirect(url_for('reviews.review', review_id=review.id))
+    elif request.method == 'GET':
+        form.title.data = review.title
+        form.content.data = review.content
+    return render_template('create_review.html', title='Update review',
+                           form=form, legend='Update review')
+
+
+@app.route('/review/<int:review_id>/delete', methods=['POST'])
+@login_required
+def delete_review(review_id):
+    review = review.query.get_or_404(review_id)
+    if review.author != current_user:
+        abort(403)
+    db.session.delete(review)
+    db.session.commit()
+    flash('Your review has been deleted.', 'danger')
+    return redirect(url_for('main.home'))
+
+
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -178,4 +270,3 @@ def logout():
 
     # Redirect user to login form
     return redirect("/")
-
